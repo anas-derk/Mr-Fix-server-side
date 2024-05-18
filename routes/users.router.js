449cc7f393c5a7c1b487c4df0357326234283812
anas derk <anas.derk2020@gmail.com>
2024-05-18 19:38:00 +0300

    Continue

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,16 +2,42 @@ const usersRouter = require("express").Router();
 
 const usersController = require("../controllers/users.controller");
 
-usersRouter.post("/create-new-user", usersController.createNewUser);
+const { validateIsExistValueForFieldsAndDataTypes } = require("../global/functions");
 
-usersRouter.get("/login", usersController.login);
+const { validateJWT, validateEmail, validatePassword, validateMobilePhone } = require("../middlewares/global.middlewares");
 
-usersRouter.get("/user-info/:userId", usersController.getUserInfo);
+usersRouter.post("/create-new-user",
+    async (req, res, next) => {
+        const emailAndPassword = req.body;
+        validateIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Email", fieldValue: emailAndPassword.email, dataType: "string", isRequiredValue: true },
+            { fieldName: "Password", fieldValue: emailAndPassword.password, dataType: "string", isRequiredValue: true },
+        ], res, next);
+    },
+    (req, res, next) => validateEmail(req.body.email, res, next),
+    (req, res, next) => validatePassword(req.body.password, res, next),
+    usersController.createNewUser
+);
+
+usersRouter.get("/login",
+    async (req, res, next) => {
+        const emailAndPassword = req.query;
+        validateIsExistValueForFieldsAndDataTypes([
+            { fieldName: "Email", fieldValue: emailAndPassword.email, dataType: "string", isRequiredValue: true },
+            { fieldName: "Password", fieldValue: emailAndPassword.password, dataType: "string", isRequiredValue: true },
+        ], res, next);
+    },
+    (req, res, next) => validateEmail(req.query.email, res, next),
+    (req, res, next) => validatePassword(req.query.password, res, next),
+    usersController.login
+);
+
+usersRouter.get("/user-info", validateJWT, usersController.getUserInfo);
 
 usersRouter.get("/forget-password", usersController.getForgetPassword);
 
 usersRouter.put("/reset-password/:userId", usersController.putResetPassword);
 
-usersRouter.put("/update-user-info/:userId", usersController.putProfile);
+usersRouter.put("/update-user-info", validateJWT, usersController.putProfile);
 
 module.exports = usersRouter;
